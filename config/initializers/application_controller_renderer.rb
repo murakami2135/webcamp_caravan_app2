90bd2a095eb0d982826f6c57e268d691a6090381
murakami2135 <mr3.213333@gmail.com>
2021-06-01 12:53:34 +0000

    app2-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a63d27b73b6c2cc60f2215ed1bef8ac19548b8004f7f151c9dc65745f98b95358e8cfbe67f969ac34f72d455aa55cb5a64e9fbc6e7fbbec7f61ae06d3664a5d1'
